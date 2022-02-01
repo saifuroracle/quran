d80a5124d1c5f8e5e0a14f437500792c87dcff41
saifur rahman <saifuroracle@gmail.com>
2022-02-01 11:00:44 +0600

    npm

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+var multer = require('multer');
+var forms = multer();
+const cors = require("cors");
+const jwt = require('jsonwebtoken');
+const {engine} = require('express-handlebars');
 const app = express()
+
 require('dotenv').config();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+// app.set('views', './views');
+
+
+// =========cors==========
+app.use(cors());
+// =========cors==========
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// for parsing multipart/form-data
+app.use(forms.array()); 
 
 
 require("./routes/routes.js")(app);
-
 
 
 const main = async () => {
