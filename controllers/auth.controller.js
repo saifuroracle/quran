5281acac765bdd77cbdd61f50b4603bca9efdb99
saifur rahman <saifuroracle@gmail.com>
2022-02-03 09:06:04 +0600

    login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,9 @@ const Users = require("../models/users.model.js");
 const { validationResult } = require('express-validator');
 const { set_response } = require('../helpers/apiresponser');
 const { mongoResult } = require('../helpers/mongohelpers');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
 
 exports.login = async (req, res) => {
     console.log('==============');
@@ -19,6 +22,22 @@ exports.login = async (req, res) => {
     if (existingUserData.status!='active') {
         return set_response(res, null, 422, 'failed', [`User is ${existingUserData.status || 'inactive'}!`])
     }
+
+    plain_password = formData.password
+    hash = existingUserData.password
+    password_validity = await bcrypt.compare(plain_password, hash)
+
+    if (!password_validity) {
+        return set_response(res, null, 422, 'failed', [`Invalid password!`])
+    }
+
+    delete existingUserData.password
+
+    var token = jwt.sign({...existingUserData }, process.env.JWT_SECRET, { expiresIn: parseInt(process.env.JWT_EXPIRES_IN) });
+
+    expires_at = moment().add(process.env.JWT_EXPIRES_IN, 'seconds').format('yy-MM-DD HH:mm:ss')
+
+
 
                                 // .exec((err, user) => {
                                 //     if(user){
