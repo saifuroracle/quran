5785fe8cf268b0d1e7b8453865e673b1a6f682ee
saifur rahman <saifuroracle@gmail.com>
2022-02-05 14:25:48 +0600

    login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,16 +5,17 @@ const { mongoResult } = require('../helpers/mongohelpers');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const moment = require('moment');
+const { unique } = require('../helpers/datahelpers');
 
 exports.login = async (req, res) => {
-    let formData = {...req.query, ...req.body}
+    let formData = { ...req.query, ...req.body }
 
     const existingUserData = await mongoResult(Users.findOne({ email: formData?.email }))
     if (!existingUserData) {
         return set_response(res, null, 422, 'failed', ['Invalid email!'])
     }
 
-    if (existingUserData.status!='active') {
+    if (existingUserData.status != 'active') {
         return set_response(res, null, 422, 'failed', [`User is ${existingUserData.status || 'inactive'}!`])
     }
 
@@ -28,43 +29,61 @@ exports.login = async (req, res) => {
 
     delete existingUserData.password
 
-    var token = jwt.sign({...existingUserData }, process.env.JWT_SECRET, { expiresIn: parseInt(process.env.JWT_EXPIRES_IN) });
+    var token = jwt.sign({ ...existingUserData }, process.env.JWT_SECRET, { expiresIn: parseInt(process.env.JWT_EXPIRES_IN) });
 
     expires_at = moment().add(process.env.JWT_EXPIRES_IN, 'seconds').format('yy-MM-DD HH:mm:ss')
 
 
     var userrolespermissions = await mongoResult(
-                                    Users
-                                    .aggregate(
-                                        [
-                                            {
-                                                $lookup: {
-                                                    from: "roles",
-                                                    localField: "role_ids",
-                                                    foreignField: "_id",
-                                                    as: "roles"
-                                                }
-                                            },
-                                            {
-                                                $unwind:  "$roles",
-                                            },
-                                            {
-                                                $lookup: {
-                                                    from: "permissions",
-                                                    localField: "roles.permission_ids",
-                                                    foreignField: "_id",
-                                                    as: "permissions",
-                                                }
-                                            },
-                                            { $match: { email: formData?.email } },
-                                            { $project: { role_ids: 0, "roles.permission_ids": 0 } }
-                                        ]
-                                    )
-                                )
+        Users
+            .aggregate(
+                [
+                    {
+                        $lookup: {
+                            from: "roles",
+                            localField: "role_ids",
+                            foreignField: "_id",
+                            as: "roles"
+                        }
+                    },
+                    {
+                        $unwind: "$roles",
+                    },
+                    {
+                        $lookup: {
+                            from: "permissions",
+                            localField: "roles.permission_ids",
+                            foreignField: "_id",
+                            as: "permissions",
+                        }
+                    },
+                    {
+                        $match: {
+                            email: formData?.email
+                        }
+                    },
+                    {
+                        $project: {
+                            role_ids: 0,
+                            "roles.permission_ids": 0
+                        }
+                    }
+                ]
+            )
+    )
+
+
+    var roles = userrolespermissions?.map(item => item?.roles?.role)  // roles data
+    var permissions = []
+    userrolespermissions?.forEach(item => {
+        const permissions_arr = item.permissions
+        permissions_arr.forEach(permission => {
+            permissions.push(permission.permission)
+        });
+    });
+    permissions = permissions.filter(unique)   // permissions data
 
     const data = userrolespermissions || {}
-    
-
 
     return set_response(res, data, 200, 'success', ['Successfully logged in'])
 };
